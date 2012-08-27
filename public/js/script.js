fb8f907495759473a1c980427ccf3cc50e72c5b1
a <a@FOSP-TUNGVV.(none)>
2012-08-27 08:13:31 +0700

    commit

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,31 @@
-now.user = $('input[name=username]').val();
-$('#create-room').click(function() {
-    now.createRoom();
+var appendNewRoom = function(room) {
+    $('#room-list').prepend($('<li />', {
+        id: 'room-' + room.id,
+        'class' : 'room',
+        html : $('<a />', {
+            href : ['',now.user,room.id].join('/'),
+            text : 'Room: ' + room.name,
+            target: '_blank'
+        })
+    }));
+};
+
+now.ready(function () {
+    now.getAllRooms(function(rooms) {
+        rooms.forEach(appendNewRoom);
+    });
+    console.log('got all rooms');
+
+    now.user = $('input[name=username]').val();
+
+    $('#create-room').click(function() {
+        var roomName = prompt('Room name');
+        now.createRoom(roomName, function onRoomCreated(room) {
+            console.log('room created', room);
+            appendNewRoom(room);
+        });
+    });
+
+
+    $('#indicator').removeClass('loading');
 });
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // require modules
 var nowjs = require("now"),
-    express = require('express');
+    express = require('express'),
+    lazy = require('lazy');
 
 // init express and http server
 var app = express();
@@ -27,9 +28,46 @@ app.get('/:user', function(req, res){
 
 app.get('/:user/:room', function(req, res) {
     console.log(req.params);
-    res.render('game/index', req.params);
+    res.render('game', req.params);
 });
 
-everyone.now.createRoom = function(callback) {
-
+var ensureFunction = function(callback) {
+    if(callback && 'function' == typeof callback) {
+        return callback;
+    }
+    return function() {};
 };
+
+everyone.now.getAllRooms = function(callback) {
+    var current = this.now;
+    var myRooms = rooms.filter(function (room) {
+        return 0 <= room.participants.indexOf(current.username);
+    });
+
+    ensureFunction(callback)(myRooms);
+};
+
+everyone.now.createRoom = function(name, callback) {
+    var room = new Room(this.now.username, name);
+    rooms.push(room);
+    var group = nowjs.getGroup(room.id);
+    callback(room);
+    console.log('room created. Name: ' + room.name + ' - id: ' + room.id);
+};
+
+var randomString = function(len) {
+  return String(Math.random()).substr(2,len);
+};
+
+var genUID = function() {
+    return [randomString(8),randomString(8)].join('-');
+};
+
+var Room = function(creator, name) {
+    this.creator = creator;
+    this.participants = [creator];
+    this.name = name || 'new room';
+    this.id = genUID();
+};
+
+var rooms = [];
